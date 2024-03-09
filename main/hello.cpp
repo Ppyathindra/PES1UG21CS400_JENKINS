090e8220e224764bc6362c48b287297067ff4a54
Ppyathindra <119135045+Ppyathindra@users.noreply.github.com>
2024-03-09 22:55:35 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-   cout << "PES1UG21CS400 I HAVE SUCEFULLY BUILT AND RUN\n";
+  cout << "PES1UG21CS400 I HAVE SUCEFULLY BUILT AND RUN\n";
   return 0;
 }
